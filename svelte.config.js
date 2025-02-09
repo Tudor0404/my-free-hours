0ba5e122758884d02f72ca34b8b2ee937869ef69
Tudor Popescu <tudor0404@gmail.com>
2025-02-09 23:04:12 +0000

    runtime change

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,21 +8,22 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
 
-
 	vitePlugin: {
 		inspector: true,
-		exclude: ["./supabase/functions/**"]
+		exclude: ['./supabase/functions/**']
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		}),
 		alias: {
 			$types: 'src/types',
 			$utils: 'src/utils',
 			$stores: 'src/stores'
-		},
+		}
 	}
 };
 export default config;
